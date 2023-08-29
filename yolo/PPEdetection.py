23d1a395f2fbecd6bf4698871a83ec819a6dbc76
凯翔 闭 <1477530671@qq.com>
2023-08-29 18:57:28 +0800

    1

--- yolo/PPEdetection.py
+++ yolo/PPEdetection.py
@@ -3,34 +3,32 @@ import cv2
 import cvzone
 import math
 import torch
-#查看版本
+
+# 查看版本
 print(torch.__version__)
-#查看gpu是否可用
+# 查看gpu是否可用
 gpuAvailable = torch.cuda.is_available()
 print(gpuAvailable)
-#返回设备gpu个数
+# 返回设备gpu个数
 gpu = torch.cuda.get_device_name()
 print(gpu)
-
 
 # cap = cv2.VideoCapture(0)  # For Webcam
 # cap.set(3, 1280)
 # cap.set(4, 720)
-cap = cv2.VideoCapture("Videos/cars.mp4")  # For Video
+cap = cv2.VideoCapture("Videos/ppe-3.mp4")  # For Video
 
-module = YOLO("YOLO_Weights/yolov8l.pt")
+module = YOLO("YOLO_Weights/HardHel.pt")
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
+# classNames = ['Excavator', 'Gloves', 'Hardhat', 'Ladder', 'Mask',
+#               'NO-Hardhat', 'NO-Mask', 'NO-Safety Vest', 'Person',
+#               'SUV', 'Safety Cone', 'Safety Vest', 'bus', 'dump truck',
+#               'fire hydrant', 'machinery', 'mini-van', 'sedan', 'semi',
+#               'trailer', 'truck and trailer', 'truck', 'van', 'vehicle',
+#               'wheel loader'
+#               ]
+classNames = ['head', 'helmet', 'person']
+myColor = (0, 0, 255)
 
 while True:
     success, img = cap.read()
@@ -43,7 +41,7 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
             w, h = x2 - x1, y2 - y1
-            cvzone.cornerRect(img, (x1, y1, w, h))
+            # cvzone.cornerRect(img, (x1, y1, w, h))
 
             #  Confidence
             conf = math.ceil(box.conf[0] * 100) / 100
@@ -51,9 +49,17 @@ while True:
 
             # Class
             cls = int(box.cls[0])
+            currentClass = classNames[cls]
+            if conf > 0.5:
+                if currentClass == "Hardhat" or currentClass == 'Safety Vest':
+                    myColor = (0, 255, 0)
+                else:
+                    myColor = (0, 0, 255)
 
-            cvzone.putTextRect(img, f"{classNames[cls]}{conf}",
-                               (max(0, x1), max(35, y1 - 10)), 2, 1, offset=4)
+                cvzone.putTextRect(img, f"{classNames[cls]}{conf}",
+                                   (max(0, x1), max(35, y1 - 10)), 1, 1,
+                                   offset=5, colorB=myColor, colorT=(255, 255, 255), colorR=myColor)
+                cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
